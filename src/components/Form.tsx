a1e574f61f6961cda0a8352f6a49f4877ccf1988
Harpalsinh Sindhav <88263558+harpalll@users.noreply.github.com>
2025-11-08 18:36:02 +0530

    L2: controlled form components

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -49,18 +49,20 @@ const elements: FormElement[] = [
 
 export const Form = ({ closeFormCB }: { closeFormCB: () => void }) => {
   const [formElements, setFormElements] = useState<FormElement[]>(elements);
+  const [newField, setNewField] = useState<string>("");
 
   const addField = () => {
     setFormElements([
       ...formElements,
       {
         id: Number(new Date()),
-        name: "Text Field",
+        name: newField,
         type: Type.Text,
-        label: "Text Field",
-        placeHolder: "Text Field",
+        label: newField,
+        placeHolder: newField,
       },
     ]);
+    setNewField("");
   };
 
   const removeField = (id: number) => {
@@ -71,7 +73,7 @@ export const Form = ({ closeFormCB }: { closeFormCB: () => void }) => {
     <>
       <h1 className="text-3xl font-bold">Form Assignment</h1>
       <div className="bg-white w-[80vh] shadow-lg flex flex-col gap-4">
-        <form className="p-2">
+        <form className="p-2 ">
           {formElements.map((element) => (
             <div className="flex justify-between items-center gap-3">
               <div
@@ -99,22 +101,34 @@ export const Form = ({ closeFormCB }: { closeFormCB: () => void }) => {
               </div>
             </div>
           ))}
+          <hr className="mt-2" />
 
-          <div className="flex gap-4">
-            <button
-              type="submit"
-              className="bg-blue-700 text-white px-4 py-2 rounded-lg mt-4 text-center font-bold"
-            >
-              Submit
-            </button>
+          <div className="flex items-center justify-between gap-3">
+            <input
+              placeholder="label for new field"
+              type="text"
+              className="border p-2 flex-1"
+              value={newField}
+              onChange={(e) => setNewField(e.target.value)}
+            />
             <button
               type="button"
-              className="bg-blue-700 text-white px-4 py-2 rounded-lg mt-4 text-center font-bold"
+              disabled={newField === ""}
+              className="bg-blue-700 text-white px-4 py-2 rounded-lg mt-4 text-center font-bold disabled:bg-gray-500"
               onClick={addField}
             >
               Add Field
             </button>
           </div>
+
+          {/* <div className="flex gap-4"> */}
+          <button
+            type="submit"
+            className="bg-blue-700 text-white px-4 py-2 rounded-lg mt-4 text-center font-bold"
+          >
+            Submit
+          </button>
+          {/* </div> */}
         </form>
 
         <button
